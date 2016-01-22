9f75b091aeba291252686ce900725376835de7f0
UtopiaSLJ <759677878@qq.com>
2016-01-22 22:32:31 +0800

    Create 一.php

---
+++ 一.php
@@ -0,0 +1,13 @@
+<html>
+ <body>
+  <head>
+     <title>留言板</title>
+  </head>
+  <body  ><H1 ALIGN=CENTER>留言小屋</H1> <hr>
+    <form  name="form1"action="二.php" method="post">
+              留言吧：<hr><input type="text" name="concent"><br>
+              <input type="submit" value="提交">
+
+     </form>
+ </body>
+</html>
