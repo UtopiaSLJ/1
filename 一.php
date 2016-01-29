f0b1842b1910856d6975acd12c447e0eeac8466a
UtopiaSLJ <759677878@qq.com>
2016-01-30 00:43:16 +0800

    Update 一.php

--- 一.php
+++ 一.php
@@ -9,5 +9,25 @@
               <input type="submit" value="提交">
 
      </form>
+     <?php
+$host=SAE_MYSQL_HOST_M SAE_MYSQL_PORT 
+$user_name=SAE_MYSQL_USER
+$password=SAE_MYSQL_PASS
+$conn = mysql_connect($host,$user_name,$password);
+if (!$conn)
+{
+    die('Could not connect: ' . mysql_error());
+}
+   echo  'Could connect! ';
+mysql_select_db("app_shilijundiyigephp");
+$sql ='select id,concent';
+$result = mysql_query($sql);
+while($row=mysql_fetch_array(result,MYSQL_ASSOC））
+{
+print_r($row);
+}
+
+mysql_close($conn);
+?>
  </body>
 </html>
