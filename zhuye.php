1b3ffd32139c1a9be5979f81732f218462451e17
UtopiaSLJ <759677878@qq.com>
2016-02-13 09:29:38 +0800

    Create zhuye.php

---
+++ zhuye.php
@@ -0,0 +1,10 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<body>
+<h1>留言小屋主页</h1>
+<a href="show.php">查看历史留言</a> | <a href="liuyan.php">留言</a>
+ <br><hr>
+    <a href="login.php">管理员入口
+
+</body>
+</html>
