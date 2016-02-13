e0bb22bed4ba85f05fe646dca40d88872b330760
UtopiaSLJ <759677878@qq.com>
2016-02-13 08:53:04 +0800

    Create liuyan.php

---
+++ liuyan.php
@@ -0,0 +1,20 @@
+<php?
+include('cookie.php');
+?>
+<html>
+ <body>
+  <head>
+     <title>留言板</title>
+  </head>
+  <body  ><H1 ALIGN=CENTER>留言小屋</H1> <hr>
+
+    <form  name="form1"action="post.php" method="post">
+             
+             留言吧： <textarea name="content" ></textarea><br>
+                         
+              <input type="submit" value="提交">
+
+     </form>
+      
+      </body>
+    </html>
