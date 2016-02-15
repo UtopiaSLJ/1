cb489d02fcb73938f08faae1c28e3d0724db10d5
UtopiaSLJ <759677878@qq.com>
2016-02-15 23:09:54 +0800

    Update liuyan.php

--- liuyan.php
+++ liuyan.php
@@ -1,18 +1,16 @@
-<php?
-include('cookie.php');
-?>
+
 <html>
  <body>
   <head>
      <title>留言板</title>
   </head>
-  <body  ><H1 ALIGN=CENTER>留言小屋</H1> <hr>
+  <body  ><H1 ALIGN=CENTER>say home</H1> <hr>
 
     <form  name="form1"action="post.php" method="post">
              
-             留言吧： <textarea name="content" ></textarea><br>
+             say： <textarea name="content" ></textarea><br>
                          
-              <input type="submit" value="提交">
+        <input type="submit"  name="submit"  />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      " value="提交"/>
 
      </form>
       
