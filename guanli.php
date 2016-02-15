2107d596e947b06cc5be383f5ef03fa9264f4f46
UtopiaSLJ <759677878@qq.com>
2016-02-15 23:08:59 +0800

    Update guanli.php

--- guanli.php
+++ guanli.php
@@ -1,24 +1,23 @@
 <php?
- $host=SAE_MYSQL_HOST_M SAE_MYSQL_PORT; 
- $user_name=SAE_MYSQL_USER;
- $password=SAE_MYSQL_PASS;
- $mysql_database    = "app_shilijundiyigephp2";
- $conn = mysql_connect($mysql_server_name,$mysql_username,$mysql_password);
-  $sql ='select id,concent from id,content' ORDER BY 'id' DESC"; 
- 
-  mysql_select_db($mysql_database,$conn);
-  $result = mysql_query($sql,$conn);?>
+ include("conn.php")
+  $sql ='select* from message' ORDER BY 'id' DESC"; 
+  $result = mysql_query($sql);
+  while($row=mysql_fetch_array($resule))
+{
+?>
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <body>
 <h1>留言本</h1>
-<a href="show.php">首页</a> | <a href="liuyan.php">留言</a><p>
-<?
-while($row=mysql_fetch_array($resule))
-{
+<a href="zhuye.php">主页</a> | <a href="liuyan.php">继续留言</a>
+<p>
+
+留言内容:<php? echo $row[content]; ?><tr>
+留言时间:<php? echo $row[lastdate]; ?>;
+
+<php?
+}
 ?>
-留言内容:<? echo $row[content] ?>
-<?}?>
-<a href="del.php?id=<?=$row[content]?>">[删除]</a>
+  <a href="del.php?id=<?=$row[0]?>">[删除]</a>  
 </body>
 </html>
