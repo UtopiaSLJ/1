ea9a10f6d9812c8cb74340a38ab88a7586a97261
UtopiaSLJ <759677878@qq.com>
2016-02-15 23:07:46 +0800

    Update del.php

--- del.php
+++ del.php
@@ -2,11 +2,13 @@
 $host=SAE_MYSQL_HOST_M SAE_MYSQL_PORT; 
 $user_name=SAE_MYSQL_USER;
 $password=SAE_MYSQL_PASS;
-  $mysql_database    = "content";
-  $sql = "DELETE FROM content WHERE id = '$id' "; 
-  $conn = mysql_connect($mysql_server_name,$mysql_username,$mysql_password);
+ $mysql_database    = "massage";
+  $sql = "DELETE FROM massage WHERE id = '$id' "; 
+   $conn = mysql_connect($mysql_server_name,$mysql_username,$mysql_password);
   mysql_select_db($mysql_database,$conn);
-  $result = mysql_query($sql,$conn);
+  $result = mysql_query($sql);
   mysql_close($conn); 
 ?>
+<p><a href="show.php">[返回]</a></p>
+
 
