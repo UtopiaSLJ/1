a302d5a24b19c77fe5df6d343190d06f067b1d2e
UtopiaSLJ <759677878@qq.com>
2016-02-16 18:02:49 +0800

    Delete login_auth.php

--- login_auth.php
+++
@@ -1,6 +0,0 @@
-<php?
-$AUTH=arry();
-$AUTH['user']='admin';
-$AUTH['password']=' 49ba59abbe56e057';                                                                                                                                                                                                                                                                                                                                                                              ';
-
-?>
