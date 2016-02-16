49e2609944af1346348469e53a075db13c6d9298
UtopiaSLJ <759677878@qq.com>
2016-02-16 18:01:28 +0800

    Update post.php

--- post.php
+++ post.php
@@ -1,17 +1,18 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 
 include("conn.php");
 
 
 if($_POST['submit']){
-
-echo  $sql ="inset into massage(id,content,lastdate)" . values('','$_POST[content]',now());
-      mysql_query($sql);
-
+   
+echo  $sql ="insert into message(id,content)" ." values('','$_POST[content]')";
+    mysql_query($sql);
+    
 }
 
 echo "<p>OK</p>";
 
 
 ?>
-<p><a href="show.php">去留言页 </a></p>         
+<p><a href="show.php">去留言页 </a></p> 
