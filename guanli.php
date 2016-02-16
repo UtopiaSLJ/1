87648dc38ab6550b8fa4a3166b1b06206d73a1ce
UtopiaSLJ <759677878@qq.com>
2016-02-16 17:57:50 +0800

    Update guanli.php

--- guanli.php
+++ guanli.php
@@ -1,24 +1,28 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<php?
- include("conn.php")
-  $sql ='select* from message' ORDER BY 'id' DESC"; 
-  $result = mysql_query($sql);
-  while($row=mysql_fetch_array($resule))
-{
-?>
 <html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <body>
 <h1>留言本</h1>
-<a href="zhuye.php">主页</a> | <a href="liuyan.php">继续留言</a>
+    <a href="zhuye.php">主页</a> | <a href="liuyan.php">留言</a>
+<?php
+ include("conn.php");
+  $sql ="select * from message ORDER BY 'id' DESC";
+  $result = mysql_query($sql);
+  while($row=mysql_fetch_array($result))
+{
+?>
+
+
 <p>
 
-留言内容:<php? echo $row[content]; ?><tr>
-留言时间:<php? echo $row[lastdate]; ?>;
-
-<php?
+留言内容:<?php echo $row['content']; ?><tr><p>
+留言时间:<?php echo $row['lastdate']; ?>;
+<a href="del.php?id=<?=$row[0]?>">[删除]</a> 
+   <hr> 
+    
+  <?php
 }
 ?>
-  <a href="del.php?id=<?=$row[0]?>">[删除]</a>  
+
 </body>
 </html>
