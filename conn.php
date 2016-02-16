ef6ac7256166911f79e8991101cce70b7364b34e
UtopiaSLJ <759677878@qq.com>
2016-02-16 17:55:10 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,13 +1,13 @@
 
-<php?
+<?php
 $conn = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS);
 if (!$conn)
 {
     die('Could not connect: ' . mysql_error());
 }
-   echo  'Could connect! ';
-mysql_select_db("app_shilijundiyigephp");
+   echo  ' ';
+mysql_select_db(app_shilijundiyigephp);
 
- $sql ="inset into cotent(id,content)"
-mysql_query($sql,$conn);
+ //$sql ="insert into message(id,content)"
+//mysql_query($sql,$conn);
 ?>
