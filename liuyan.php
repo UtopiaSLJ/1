d8e621cc8e31bf7c5de83288bc6e876909aae396
UtopiaSLJ <759677878@qq.com>
2016-02-16 17:58:43 +0800

    Update liuyan.php

--- liuyan.php
+++ liuyan.php
@@ -1,16 +1,17 @@
-
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <html>
  <body>
   <head>
      <title>留言板</title>
   </head>
-  <body  ><H1 ALIGN=CENTER>say home</H1> <hr>
+  <body  ><H1 ALIGN=CENTER>留言小屋</H1> <hr>
 
     <form  name="form1"action="post.php" method="post">
              
-             say： <textarea name="content" ></textarea><br>
+             留言吧： <textarea name="content" ></textarea><br>
+     
                          
-        <input type="submit"  name="submit"  />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      " value="提交"/>
+        <input type="submit"  name="submit" value="提交留言" />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
      </form>
       
