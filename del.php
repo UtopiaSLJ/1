7310da9ed4689f857e69179edc1e8b85580b8f9a
UtopiaSLJ <759677878@qq.com>
2016-02-16 17:55:48 +0800

    Update del.php

--- del.php
+++ del.php
@@ -1,14 +1,18 @@
-<?
-$host=SAE_MYSQL_HOST_M SAE_MYSQL_PORT; 
-$user_name=SAE_MYSQL_USER;
-$password=SAE_MYSQL_PASS;
- $mysql_database    = "massage";
-  $sql = "DELETE FROM massage WHERE id = '$id' "; 
-   $conn = mysql_connect($mysql_server_name,$mysql_username,$mysql_password);
-  mysql_select_db($mysql_database,$conn);
-  $result = mysql_query($sql);
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+ $id=$_GET['id'];
+ $conn = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS);
+
+  mysql_select_db(app_shilijundiyigephp,$conn); 
+ $sql = "DELETE FROM message WHERE id = '$id' "; 
+  
+  mysql_query($sql);
   mysql_close($conn); 
 ?>
-<p><a href="show.php">[返回]</a></p>
+<a href="guanli.php">[返回]</a>
+
+
+
+
 
 
