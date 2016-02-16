73aac60e9c0565769ca0a63637e908882886faf5
UtopiaSLJ <759677878@qq.com>
2016-02-16 18:01:53 +0800

    Update show.php

--- show.php
+++ show.php
@@ -1,21 +1,23 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<php?
- include("conn.php")
-  $sql ='select* from message' ORDER BY 'id' DESC"; 
-  $result = mysql_query($sql);
-  while($row=mysql_fetch_array($resule))
-{
-?>
 <html>
-
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <body>
 <h1>留言本</h1>
-<a href="zhuye.php">主页</a> | <a href="liuyan.php">去添加留言</a>
+    <a href="zhuye.php">主页</a> | <a href="liuyan.php">留言</a>
+<?php
+ include("conn.php");
+  $sql ="select * from message ORDER BY 'id' DESC";
+  $result = mysql_query($sql);
+  while($row=mysql_fetch_array($result))
+{
+?>
+
+
 <p>
 
-留言内容:<php? echo $row[content]; ?><tr>
-留言时间:<php? echo $row[lastdate]; ?>;
-<php?
+留言内容:<?php echo $row['content']; ?><tr><p>
+留言时间:<?php echo $row['lastdate']; ?>
+    <hr>
+<?php
 }
 ?>
 </body>
